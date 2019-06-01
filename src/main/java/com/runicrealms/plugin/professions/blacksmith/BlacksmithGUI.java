9d3589dd10ba56e4f329580125f6bf43b07d256e
OhCreative <OhCreativeYT@gmail.com>
2019-06-01 16:15:39 -0400

    Fixing Professions Inheritence (pls no break java convention...)

--- src/main/java/com/runicrealms/plugin/professions/blacksmith/BlacksmithGUI.java
+++ src/main/java/com/runicrealms/plugin/professions/blacksmith/BlacksmithGUI.java
@@ -5,19 +5,20 @@ import com.runicrealms.plugin.item.ItemGUI;
 import com.runicrealms.plugin.item.OptionClickEvent;
 import com.runicrealms.plugin.professions.Workstation;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-public class BlacksmithGUI extends Workstation {
+public class BlacksmithGUI extends ItemGUI implements Workstation {
 
     public BlacksmithGUI(Player pl) {
-
         super(pl.getName() + "'s Blacksmith Workstation", 27, (OptionClickEvent event) -> {
 
-            Bukkit.broadcastMessage(event.getPosition() + "");
+            Bukkit.broadcastMessage(event.getSlot() + "");
 
             // open skin editor
-            if (event.getPosition() == 5 || event.getPosition() == 4) {
+            if (event.getSlot() == 5 || event.getSlot() == 4) {
                 pl.playSound(pl.getLocation(), Sound.UI_BUTTON_CLICK, 0.5f, 1);
                 //ItemGUI skinEditor = ArtifactGUI.skinEditor(pl, artifact, durability);
                 //skinEditor.open(pl);
@@ -28,11 +29,16 @@ public class BlacksmithGUI extends Workstation {
 
         super.setHandler((OptionClickEvent eve) -> {
 
-            if (eve.getSuper().getSlot() == 0) {
+            if (eve.getSlot() == 0) {
                 Bukkit.broadcastMessage("you win!");
             }
         });
 
-        super.createCraftableItem();
+        createCraftableItem();
+    }
+
+    @Override
+    public void createCraftableItem() {
+        super.setOption(5, new ItemStack(Material.DIAMOND), "&aDiamond", "&7Test", 0);
     }
 }
