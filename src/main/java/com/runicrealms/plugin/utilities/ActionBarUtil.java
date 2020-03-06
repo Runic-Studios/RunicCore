0fbfeb106f891f3f25daaf7399bc860b61ebc4ce
Skyfallin <zextertech777@yahoo.com>
2020-03-06 11:32:07 -0700

    action bar util fixed

--- src/main/java/com/runicrealms/plugin/utilities/ActionBarUtil.java
+++ src/main/java/com/runicrealms/plugin/utilities/ActionBarUtil.java
@@ -1,25 +1,105 @@
 package com.runicrealms.plugin.utilities;
 
 import com.runicrealms.plugin.RunicCore;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class ActionBarUtil {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
-    public static void sendTimedMessage(Player pl, String message, int seconds) {
-        new BukkitRunnable() {
-            long startTime = System.currentTimeMillis();
-            @Override
-            public void run() {
-                long time = System.currentTimeMillis();
-                if ((time - startTime) >= seconds*1000) {
-                    this.cancel();
-                } else {
-                    pl.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ColorUtil.format(message)));
+public class ActionBarUtil {
+//    public static void sendTimedMessage(Player pl, String message, int seconds) {
+//        new BukkitRunnable() {
+//            long startTime = System.currentTimeMillis();
+//            @Override
+//            public void run() {
+//                long time = System.currentTimeMillis();
+//                if ((time - startTime) >= seconds*1000) {
+//                    this.cancel();
+//                } else {
+//                    pl.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ColorUtil.format(message)));
+//                }
+//            }
+//        }.runTaskTimer(RunicCore.getInstance(), 0, 10L);
+//    }
+
+    private static void sendActionBar(Player player, String message) {
+
+        /*private static*/ String nmsver = Bukkit.getServer().getClass().getPackage().getName();
+        nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
+
+        if (!player.isOnline()) {
+            return; // Player may have logged out
+        }
+
+        // Call the event, if cancelled don't send Action Bar
+
+        try {
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
+            Object packet;
+            Class<?> packetPlayOutChatClass = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
+            Class<?> packetClass = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+
+                Class<?> chatComponentTextClass = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                try {
+                    Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
+                    Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+                    Object chatMessageType = null;
+                    for (Object obj : chatMessageTypes) {
+                        if (obj.toString().equals("GAME_INFO")) {
+                            chatMessageType = obj;
+                        }
+                    }
+                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, chatMessageTypeClass}).newInstance(chatCompontentText, chatMessageType);
+                } catch (ClassNotFoundException cnfe) {
+                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(chatCompontentText, (byte) 2);
                 }
-            }
-        }.runTaskTimerAsynchronously(RunicCore.getInstance(), 0, 1L);
+
+            Method craftPlayerHandleMethod = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object craftPlayerHandle = craftPlayerHandleMethod.invoke(craftPlayer);
+            Field playerConnectionField = craftPlayerHandle.getClass().getDeclaredField("playerConnection");
+            Object playerConnection = playerConnectionField.get(craftPlayerHandle);
+            Method sendPacketMethod = playerConnection.getClass().getDeclaredMethod("sendPacket", packetClass);
+            sendPacketMethod.invoke(playerConnection, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Send the player an action bar message with a duration.
+     * @param duration in SECONDS (method converts to TICKS)
+     */
+    public static void sendTimedMessage(final Player player, final String message, int duration) {
+
+        duration *= 20; // convert to ticks
+
+        sendActionBar(player, message);
+
+        if (duration >= 0) {
+            // Sends empty message at the end of the duration. Allows messages shorter than 3 seconds, ensures precision.
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, "");
+                }
+            }.runTaskLater(RunicCore.getInstance(), duration + 1);
+        }
+
+        // Re-sends the messages every 3 seconds so it doesn't go away from the player's screen.
+        while (duration > 40) {
+            duration -= 40;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, message);
+                }
+            }.runTaskLater(RunicCore.getInstance(), duration);
+        }
     }
 }
