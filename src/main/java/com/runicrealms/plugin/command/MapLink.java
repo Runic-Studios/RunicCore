093f83591447f6132e3d32d179261f9d4a2c1ac1
Excel619 <kai@enoshlo.com>
2020-04-01 17:16:25 +0300

    Added color codes

--- src/main/java/com/runicrealms/plugin/command/MapLink.java
+++ src/main/java/com/runicrealms/plugin/command/MapLink.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,8 +13,7 @@ public class MapLink implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] strings) {
-        sender.spigot().sendMessage();
-        BaseComponent component = new TextComponent("Click here to open the Runic Realms online map: https://runicrealms.com/map");
+        BaseComponent component = new TextComponent(ChatColor.GREEN + "Click here to open the Runic Realms online map: " + ChatColor.DARK_GREEN + "https://runicrealms.com/map");
         component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://runicrealms.com/map"));
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[] {new TextComponent("Runic Realms Map")}));
         sender.spigot().sendMessage(component);
