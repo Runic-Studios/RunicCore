fbf2c3a2c66a675ef90ebb67096066c97ada3a23
Skyfallin <zextertech777@yahoo.com>
2020-04-08 00:06:28 -0700

    removed old item serialization methods

--- src/main/java/com/runicrealms/plugin/database/util/DatabaseUtil.java
+++ src/main/java/com/runicrealms/plugin/database/util/DatabaseUtil.java
@@ -1,14 +1,16 @@
 package com.runicrealms.plugin.database.util;
 
-import com.runicrealms.plugin.RunicCore;
 import com.runicrealms.runiccharacters.config.UserConfig;
-import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.libs.org.apache.commons.io.output.ByteArrayOutputStream;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.IOException;
 
 public class DatabaseUtil {
 
@@ -27,40 +29,40 @@ public class DatabaseUtil {
      * @param userConfig from RunicCharacters
      */
     public static ItemStack[] loadInventory(UserConfig userConfig) {
-        ItemStack[] contents = new ItemStack[41];
-        try {
-            Player pl = userConfig.getPlayer();
-            Document playerFile = RunicCore.getDatabaseManager().getAPI().getPlayerFile("2343243243243");//pl.getUniqueId().toString()
-            Document playerCharacter = RunicCore.getDatabaseManager().getAPI().getCharacterAPI().getCharacter(playerFile, 1);
-            String serialized = playerCharacter.getString("inventory");
-            YamlConfiguration restoreInv = new YamlConfiguration();
-            restoreInv.loadFromString(serialized);
-            for (int i = 0; i < 41; i++) {
-                ItemStack restored = restoreInv.getItemStack(String.valueOf(i));
-                if (restored != null) {
-                    contents[i] = restored;
-                }
-            }
-            return contents;
-        } catch (Exception e) {
+//        ItemStack[] contents = new ItemStack[41];
+//        try {
+//            Player pl = userConfig.getPlayer();
+//            Document playerFile = RunicCore.getDatabaseManager().getAPI().getPlayerFile("2343243243243");//pl.getUniqueId().toString()
+//            Document playerCharacter = RunicCore.getDatabaseManager().getAPI().getCharacterAPI().getCharacter(playerFile, 1);
+//            String serialized = playerCharacter.getString("inventory");
+//            YamlConfiguration restoreInv = new YamlConfiguration();
+//            restoreInv.loadFromString(serialized);
+//            for (int i = 0; i < 41; i++) {
+//                ItemStack restored = restoreInv.getItemStack(String.valueOf(i));
+//                if (restored != null) {
+//                    contents[i] = restored;
+//                }
+//            }
+//            return contents;
+//        } catch (Exception e) {
             return new ItemStack[41];
-        }
+//        }
     }
 
     public static Location loadLocation(UserConfig userConfig) {
-        try {
-            Player pl = userConfig.getPlayer();
-            Document playerFile = RunicCore.getDatabaseManager().getAPI().getPlayerFile("2343243243243");//pl.getUniqueId().toString()
-            Document playerCharacter = RunicCore.getDatabaseManager().getAPI().getCharacterAPI().getCharacter(playerFile, 1);
-            String serialized = playerCharacter.getString("location");
-            YamlConfiguration restoreLoc = new YamlConfiguration();
-            restoreLoc.loadFromString(serialized);
-            return restoreLoc.getLocation("loc");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Bukkit.broadcastMessage("something went wrong");
+//        try {
+//            Player pl = userConfig.getPlayer();
+//            Document playerFile = RunicCore.getDatabaseManager().getAPI().getPlayerFile("2343243243243");//pl.getUniqueId().toString()
+//            Document playerCharacter = RunicCore.getDatabaseManager().getAPI().getCharacterAPI().getCharacter(playerFile, 1);
+//            String serialized = playerCharacter.getString("location");
+//            YamlConfiguration restoreLoc = new YamlConfiguration();
+//            restoreLoc.loadFromString(serialized);
+//            return restoreLoc.getLocation("loc");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            Bukkit.broadcastMessage("something went wrong");
             return new Location(Bukkit.getWorld("Alterra"), -2317.5, 38.5, 1719.5);
-        }
+//        }
     }
 
     /**
@@ -75,6 +77,19 @@ public class DatabaseUtil {
             invConfig.set(String.valueOf(i), is);
         }
         return invConfig.saveToString();
+    }
+
+    private static String serializeItemStack(ItemStack item) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            dataOutput.writeObject(item);
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 
     /**
