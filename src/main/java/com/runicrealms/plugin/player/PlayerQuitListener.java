19bc1726a5bfe80f388b5557324e42906d0ba538
Skyfallin <zextertech777@yahoo.com>
2020-04-18 22:49:15 -0700

    player quit is now async!

--- src/main/java/com/runicrealms/plugin/player/PlayerQuitListener.java
+++ src/main/java/com/runicrealms/plugin/player/PlayerQuitListener.java
@@ -4,20 +4,26 @@ import com.runicrealms.plugin.RunicCore;
 import com.runicrealms.plugin.player.cache.PlayerCache;
 import com.runicrealms.runiccharacters.api.RunicCharactersApi;
 import com.runicrealms.runiccharacters.api.events.CharacterQuitEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+/**
+ * Saves player data async
+ */
 public class PlayerQuitListener implements Listener {
 
     @EventHandler
     public void onLoadedQuit(CharacterQuitEvent e) {
-        // get player cache (if they've loaded in)
-        Player pl = e.getPlayer();
-        if (RunicCore.getCacheManager().getPlayerCache(pl.getUniqueId()) != null) {
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(RunicCore.getInstance(),
+                () -> saveCharacterRemoveQueue(e.getPlayer()), 1L);
+    }
 
-            //Bukkit.broadcastMessage("new character loading! saving files");
+    private void saveCharacterRemoveQueue(Player pl) {
+        // get player cache (if they've loaded in)
+        if (RunicCore.getCacheManager().getPlayerCache(pl.getUniqueId()) != null) {
 
             PlayerCache playerCache = RunicCore.getCacheManager().getPlayerCache(pl.getUniqueId());
 
