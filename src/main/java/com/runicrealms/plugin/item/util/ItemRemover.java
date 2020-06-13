e906e77add6f5243c04441c8dfb50e4b1d565987
Skyfallin <dimetrius.hightower@gmail.com>
2020-06-13 15:15:41 -0700

    prevent non-offhands from being equipped

--- src/main/java/com/runicrealms/plugin/item/util/ItemRemover.java
+++ src/main/java/com/runicrealms/plugin/item/util/ItemRemover.java
@@ -1,6 +1,5 @@
 package com.runicrealms.plugin.item.util;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -12,20 +11,8 @@ public class ItemRemover {
      */
     public static void takeItem(Player pl, ItemStack itemStack, int amount) {
         int to_take = amount;
-
-        if (pl.getInventory().getItemInOffHand().isSimilar(itemStack)) {
-            Bukkit.broadcastMessage("here");
-            int take_next = Math.min(to_take, pl.getInventory().getItemInOffHand().getAmount());
-            remove(pl, pl.getInventory().getItemInOffHand(), take_next);
-            to_take -= take_next;
-            if (to_take <= 0) { //Reached amount. Can stop!
-                return;
-            }
-        }
-
         for (ItemStack player_item : pl.getInventory().getContents()) {
             if (player_item != null) {
-
                 if (player_item.isSimilar(itemStack)) {
                     int take_next = Math.min(to_take, player_item.getAmount());
                     remove(pl, player_item, take_next);
@@ -45,7 +32,6 @@ public class ItemRemover {
         int to_take = amount;
         for (ItemStack player_item : pl.getInventory().getContents()) {
             if (player_item != null) {
-
                 if (player_item.getType() == material) {
                     int take_next = Math.min(to_take, player_item.getAmount());
                     remove(pl, player_item, take_next);
