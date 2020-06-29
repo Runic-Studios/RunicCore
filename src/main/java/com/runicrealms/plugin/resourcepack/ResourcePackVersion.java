7d90a5e871fbf38c3185560c53af88c5175e0a1e
Skyfallin <dimetrius.hightower@gmail.com>
2020-06-28 19:49:18 -0700

    resource pack version bugfix

--- src/main/java/com/runicrealms/plugin/resourcepack/ResourcePackVersion.java
+++ src/main/java/com/runicrealms/plugin/resourcepack/ResourcePackVersion.java
@@ -2,14 +2,14 @@ package com.runicrealms.plugin.resourcepack;
 
 public enum ResourcePackVersion {
 
-    MC_1_9("https://www.dropbox.com/s/mlfr5lc8hgay1dm/1.10_RR.zip?dl=1", 48, 110), // same as 1.10
-    MC_1_10("https://www.dropbox.com/s/mlfr5lc8hgay1dm/1.10_RR.zip?dl=1", 201, 210),
-    MC_1_11("https://www.dropbox.com/s/c106voh829ozs3x/1.12_RR.zip?dl=1", 301, 316), // same as 1.12 // https://www.dropbox.com/s/b7hu3aqlehzd135/1.11_RR.zip?dl=1
-    MC_1_12("https://www.dropbox.com/s/c106voh829ozs3x/1.12_RR.zip?dl=1", 317, 340),
-    MC_1_13("https://www.dropbox.com/s/wl1kh91ck2w68lt/1.13_RR.zip?dl=1", 341, 404), // uses old font format
-    MC_1_14("https://www.dropbox.com/s/20hz2r5e44fvtt8/1.14_RR.zip?dl=1", 441, 498),
-    MC_1_15("https://www.dropbox.com/s/0aun58ahh2eq2v0/1.15_RR.zip?dl=1", 550, 578),
-    MC_1_16("https://www.dropbox.com/s/f8scwmjth80b1tr/1.16_RR.zip?dl=1", 701, 1000); // new font format (again)
+    MC_1_9("https://www.dropbox.com/s/hchbwwismlkzjll/1.10_RR.zip?dl=1", 48, 110), // same as 1.10
+    MC_1_10("https://www.dropbox.com/s/hchbwwismlkzjll/1.10_RR.zip?dl=1", 201, 210),
+    MC_1_11("https://www.dropbox.com/s/ydrvb8ia2skfgho/1.12_RR.zip?dl=1", 301, 316), // same as 1.12 // https://www.dropbox.com/s/b7hu3aqlehzd135/1.11_RR.zip?dl=1
+    MC_1_12("https://www.dropbox.com/s/ydrvb8ia2skfgho/1.12_RR.zip?dl=1", 317, 340),
+    MC_1_13("https://www.dropbox.com/s/pmr3nzcqm8mul36/1.13_RR.zip?dl=1", 341, 404), // uses old font format
+    MC_1_14("https://www.dropbox.com/s/001c0p9ghlk5380/1.14_RR.zip?dl=1", 441, 498),
+    MC_1_15("https://www.dropbox.com/s/i01fb845sddkxb4/1.15_RR.zip?dl=1", 550, 578),
+    MC_1_16("https://www.dropbox.com/s/wicinwsnyt217n3/1.16_RR.zip?dl=1", 701, 1000); // new font format (again)
 
     private final String link;
     private final Integer minVersion;
