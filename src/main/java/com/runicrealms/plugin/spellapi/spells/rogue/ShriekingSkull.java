ea8f360524fc280aae687acbf866cc1aaf6cc14c
Skyfallin <dimetrius.hightower@gmail.com>
2020-08-06 12:43:06 -0700

    fixed shrieking skull glitch

--- src/main/java/com/runicrealms/plugin/spellapi/spells/rogue/ShriekingSkull.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/rogue/ShriekingSkull.java
@@ -1,5 +1,6 @@
 package com.runicrealms.plugin.spellapi.spells.rogue;
 
+import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
 import com.runicrealms.plugin.RunicCore;
 import com.runicrealms.plugin.classes.ClassEnum;
 import com.runicrealms.plugin.spellapi.spelltypes.Spell;
@@ -12,16 +13,10 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.WitherSkull;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class ShriekingSkull extends Spell {
@@ -30,7 +25,6 @@ public class ShriekingSkull extends Spell {
     private static final double LAUNCH_MULT = 1.5;
     private static final double SKULL_SPEED = 0.8;
     private WitherSkull skull;
-    private final List<WitherSkull> hit = new ArrayList<>();
 
     public ShriekingSkull() {
         super ("Shrieking Skull",
@@ -63,29 +57,20 @@ public class ShriekingSkull extends Spell {
         }.runTaskTimer(RunicCore.getInstance(), 0, 1L);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onSkullDamage(EntityDamageByEntityEvent event) {
+    @EventHandler
+    public void onTridentDamage(ProjectileCollideEvent e) {
 
-        // only listen for our fireball
-        if (!(event.getDamager().equals(this.skull))
-                || event.getDamager() instanceof WitherSkull
-                && hit.contains(event.getDamager())) return;
-
-        // skip the caster
-        if (event.getEntity() == skull.getShooter()) return;
-
-        event.setCancelled(true);
-
-        // prevent multiple enemies from being hit
-        event.getDamager().remove();
-        hit.add((WitherSkull) event.getDamager());
+        if (!e.getEntity().equals(this.skull)) return;
+        e.setCancelled(true);
+        e.getEntity().remove();
+        e.getEntity().remove();
 
         // grab our variables
         Player player = (Player) skull.getShooter();
-        LivingEntity victim = (LivingEntity) event.getEntity();
+        LivingEntity victim = (LivingEntity) e.getCollidedWith();
         if (player == null) return;
 
-       if (!verifyEnemy(player, victim)) return;
+        if (!verifyEnemy(player, victim)) return;
 
         // cancel the event, apply spell mechanics
         Vector launch = new Vector(0, 10.0f, 0).normalize().multiply(LAUNCH_MULT);
@@ -99,16 +84,6 @@ public class ShriekingSkull extends Spell {
         victim.getWorld().playSound(victim.getLocation(), Sound.ENTITY_ENDER_DRAGON_FLAP, 1.0f, 1.0f);
         victim.getWorld().spawnParticle(Particle.REDSTONE, victim.getLocation(),
                 15, 0.5f, 0.5f, 0.5f, new Particle.DustOptions(Color.BLACK, 2));
-    }
-
-    /**
-     * Prevent block damage, AoE
-     */
-    @EventHandler
-    public void onBlockDamage(ExplosionPrimeEvent e) {
-        if (e.getEntity() == this.skull) {
-            e.setCancelled(true);
-        }
     }
 }
 
