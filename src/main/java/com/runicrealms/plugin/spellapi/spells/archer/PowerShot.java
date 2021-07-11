32910d2fabc129484ee16f870e290087454f5b8d
Skyfallin <dimetrius.hightower@gmail.com>
2021-07-10 21:36:20 -0700

    powershot is now aoe, and twist of fate logic has been implemented! testing to follow!

--- src/main/java/com/runicrealms/plugin/spellapi/spells/archer/PowerShot.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/archer/PowerShot.java
@@ -31,16 +31,17 @@ public class PowerShot extends Spell implements MagicDamageSpell {
     private static final int DAMAGE_PER_LEVEL = 4;
     private static final int DURATION = 6;
     private static final double PERCENT_INCREASE = .25;
+    private static final int RADIUS = 3;
     private final List<Arrow> powerShots;
     private static final HashMap<UUID, UUID> markedEntities = new HashMap<>();
 
-    // todo: has arrow bomb effect, radius = 3
     public PowerShot() {
         super("Power Shot",
                 "You launch an enchanted arrow which " +
                         "deals (" + DAMAGE + " + &f" + DAMAGE_PER_LEVEL
-                        + "x&7 lvl)" + " spellʔ damage on-hit! " +
-                        "For " + DURATION + "s, the enemy is afflicted " +
+                        + "x&7 lvl)" + " spellʔ damage on-hit to " +
+                        "all enemies within " + RADIUS + " blocks! " +
+                        "For " + DURATION + "s, enemies hit by this spell are afflicted " +
                         "with &aHunter's Mark&7, increasing all damage you " +
                         "deal to them by " + (int) (PERCENT_INCREASE * 100) + "%!",
                 ChatColor.WHITE, ClassEnum.ARCHER, 12, 25);
@@ -70,7 +71,13 @@ public class PowerShot extends Spell implements MagicDamageSpell {
                     this.cancel();
                     pl.getWorld().playSound(pl.getLocation(), Sound.BLOCK_LAVA_POP, 0.5f, 2f);
                     pl.getWorld().playSound(pl.getLocation(), Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 0.5f, 2f);
+                    powerShot.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, arrowLoc, 10, 0, 0, 0, 0);
                     powerShot.getWorld().spawnParticle(Particle.CRIT_MAGIC, arrowLoc, 25, 0.5f, 0.5f, 0.5f, 0);
+                    for (Entity entity : pl.getWorld().getNearbyEntities(arrowLoc, RADIUS, RADIUS, RADIUS)) {
+                        if (!(entity instanceof LivingEntity)) continue;
+                        if (!verifyEnemy(pl, entity)) continue;
+                        applyHuntersMark(pl, (LivingEntity) entity);
+                    }
                 }
             }
         }.runTaskTimer(RunicCore.getInstance(), 0, 1L);
@@ -86,18 +93,24 @@ public class PowerShot extends Spell implements MagicDamageSpell {
         if (!powerShots.contains(arrow)) return;
 
         e.setCancelled(true);
+//        if (!(e.getEntity() instanceof LivingEntity)) return;
+//        Player pl = (Player) ((Arrow) e.getDamager()).getShooter();
+//        assert pl != null;
+//        LivingEntity le = (LivingEntity) e.getEntity();
+//
+//        if (verifyEnemy(pl, le)) {
+//            DamageUtil.damageEntitySpell(DAMAGE, le, pl, this);
+//            markedEntities.put(pl.getUniqueId(), le.getUniqueId());
+//            Cone.coneEffect(le, Particle.REDSTONE, DURATION, 0, 20L, Color.GREEN);
+//            Bukkit.getScheduler().scheduleAsyncDelayedTask(RunicCore.getInstance(), () -> markedEntities.remove(pl.getUniqueId()), DURATION * 20L);
+//        }
+    }
 
-        if (!(e.getEntity() instanceof LivingEntity)) return;
-        Player pl = (Player) ((Arrow) e.getDamager()).getShooter();
-        assert pl != null;
-        LivingEntity le = (LivingEntity) e.getEntity();
-
-        if (verifyEnemy(pl, le)) {
-            DamageUtil.damageEntitySpell(DAMAGE, le, pl, this);
-            markedEntities.put(pl.getUniqueId(), le.getUniqueId());
-            Cone.coneEffect(le, Particle.REDSTONE, DURATION, 0, 20L, Color.GREEN);
-            Bukkit.getScheduler().scheduleAsyncDelayedTask(RunicCore.getInstance(), () -> markedEntities.remove(pl.getUniqueId()), DURATION * 20L);
-        }
+    private void applyHuntersMark(Player pl, LivingEntity le) {
+        DamageUtil.damageEntitySpell(DAMAGE, le, pl, this);
+        markedEntities.put(pl.getUniqueId(), le.getUniqueId());
+        Cone.coneEffect(le, Particle.REDSTONE, DURATION, 0, 20L, Color.GREEN);
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(RunicCore.getInstance(), () -> markedEntities.remove(pl.getUniqueId()), DURATION * 20L);
     }
 
     @EventHandler
--- src/main/java/com/runicrealms/plugin/spellapi/spells/rogue/TwistOfFate.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/rogue/TwistOfFate.java
@@ -2,117 +2,132 @@ package com.runicrealms.plugin.spellapi.spells.rogue;
 
 import com.runicrealms.plugin.RunicCore;
 import com.runicrealms.plugin.classes.ClassEnum;
-import com.runicrealms.plugin.spellapi.spelltypes.EffectEnum;
+import com.runicrealms.plugin.events.HealthRegenEvent;
+import com.runicrealms.plugin.events.SpellHealEvent;
 import com.runicrealms.plugin.spellapi.spelltypes.Spell;
 import com.runicrealms.plugin.spellapi.spelltypes.SpellItemType;
-import org.bukkit.*;
+import com.runicrealms.plugin.utilities.DamageUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Firework;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class TwistOfFate extends Spell {
 
-    private static final int DURATION = 5;
-    private static final int MAX_DIST = 10;
-    private static final int RADIUS = 5;
-    private Firework firework;
-    private final HashSet<UUID> invertedPlayers;
+    private static final int BUBBLE_DURATION = 6;
+    private static final int BUBBLE_SIZE = 5;
+    private static final double UPDATES_PER_SECOND = 10;
+    private final HashSet<UUID> fateCasters;
+    private final HashMap<UUID, Player> invertedEntities;
 
-    // todo: finish
     public TwistOfFate() {
-        super ("Twist of Fate",
-                "You cast a wicked curse of inversion, " +
-                        "striking all enemies within " + RADIUS +
-                        " blocks! Enemies affected by the curse " +
-                        "have their restoring✦ effects inverted " +
-                        "for " + DURATION + "s, suffering damage " +
-                        "instead! Against monsters, this ability will " +
-                        "silence for the duration.",
-                ChatColor.WHITE, ClassEnum.ROGUE, 1, 35); // cd: 40
-        invertedPlayers = new HashSet<>();
+        super("Twist of Fate",
+                "For " + BUBBLE_DURATION + "s, you conjure a " +
+                        "wicked sphere of inversion, affecting " +
+                        "all enemies within " + BUBBLE_SIZE + " blocks! " +
+                        "Enemies who enter the sphere are debuffed for the " +
+                        "duration of this spell and cannont be healed✦, " +
+                        "suffering spellʔ damage instead! Against monsters, " +
+                        "this ability will silence for the duration.",
+                ChatColor.WHITE, ClassEnum.ROGUE, 1, 40); // todo: cd: 45
+        fateCasters = new HashSet<>();
+        invertedEntities = new HashMap<>();
     }
 
     @Override
-    public void executeSpell(Player player, SpellItemType type) {
-        // todo: wither death sound? ender dragon death sound?
-        Location location = player.getLocation();
-        player.getWorld().spigot().strikeLightningEffect(location, true);
-        BukkitRunnable rain = new BukkitRunnable() {
-            int count = 1;
+    public boolean attemptToExecute(Player pl) {
+        if (!pl.isOnGround()) {
+            pl.sendMessage(ChatColor.RED + "You must be on the ground to cast " + this.getName() + "!");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void executeSpell(Player pl, SpellItemType type) {
+
+        // Play sound effects
+        pl.getWorld().playSound(pl.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_IMPACT, 0.5F, 1.0F);
+        pl.getWorld().playSound(pl.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 0.5F, 1.0F);
+        pl.getWorld().spigot().strikeLightningEffect(pl.getLocation(), true);
+        fateCasters.add(pl.getUniqueId());
+
+        // Begin spell event
+        final long startTime = System.currentTimeMillis();
+        new BukkitRunnable() {
+            double phi = 0;
+
             @Override
             public void run() {
-                if (count > DURATION)
+
+                // create visual bubble
+                phi += Math.PI / 10;
+                Location loc = pl.getLocation();
+                for (double theta = 0; theta <= 2 * Math.PI; theta += Math.PI / 40) {
+                    double x = BUBBLE_SIZE * cos(theta) * sin(phi);
+                    double y = BUBBLE_SIZE * cos(phi) + 1.5;
+                    double z = BUBBLE_SIZE * sin(theta) * sin(phi);
+                    loc.add(x, y, z);
+                    pl.getWorld().spawnParticle(Particle.SMOKE_NORMAL, loc, 1, 0, 0, 0, 0);
+                    loc.subtract(x, y, z);
+                }
+
+                // Spell duration, allow cancel by sneaking
+                long timePassed = System.currentTimeMillis() - startTime;
+                if (timePassed > BUBBLE_DURATION * 1000 || pl.isSneaking()) {
                     this.cancel();
-                else {
-                    count += 1;
-                    spawnRing(player, location);
+                    fateCasters.clear();
+                    invertedEntities.clear();
+                    return;
+                }
+
+                // More effect noises
+                pl.getWorld().playSound(pl.getLocation(), Sound.ENTITY_CAT_HISS, 0.01F, 0.5F);
+
+                // Look for targets nearby
+                for (Entity entity : pl.getNearbyEntities(BUBBLE_SIZE, BUBBLE_SIZE, BUBBLE_SIZE)) {
+                    if (!verifyEnemy(pl, entity)) continue;
+                    // todo: particle
+                    // todo: silence if monster
+                    invertedEntities.put(entity.getUniqueId(), pl);
                 }
             }
-        };
-        rain.runTaskTimer(RunicCore.getInstance(), 0, 20L);
+        }.runTaskTimer(RunicCore.getInstance(), 0, (int) (20 / UPDATES_PER_SECOND));
     }
 
-    /**
-     *
-     * @param pl
-     * @param loc
-     * @return
-     */
-    private Location spawnRing(Player pl, Location loc) {
-
-        pl.getWorld().playSound(pl.getLocation(), Sound.WEATHER_RAIN, 0.5F, 1.0F);
-
-        int particles = 50;
-        float radius = RADIUS;
-
-        // create circle
-        for (int i = 0; i < particles; i++) {
-            double angle, x, z;
-            angle = 2 * Math.PI * i / particles;
-            x = Math.cos(angle) * radius;
-            z = Math.sin(angle) * radius;
-            loc.add(x, 0, z);
-            pl.getWorld().spawnParticle(Particle.REDSTONE, loc, 5, 0, 0, 0, 0,
-                    new Particle.DustOptions(Color.TEAL, 1));
-            pl.getWorld().spawnParticle(Particle.CRIT_MAGIC, loc, 5, 0, 0, 0, 0);
-            loc.subtract(x, 0, z);
-        }
-
-        int ranX = ThreadLocalRandom.current().nextInt(-(RADIUS/2), RADIUS/2 + 1);
-        int ranZ = ThreadLocalRandom.current().nextInt(-(RADIUS/2), RADIUS/2 + 1);
-
-        Location tempSplash = new Location(pl.getWorld(), loc.getX()+ranX, loc.getY(), loc.getZ()+ranZ);
-
-        pl.getWorld().spawnParticle(Particle.WATER_SPLASH, tempSplash, 25, 0, 0, 0); // todo: lightning
-        pl.getWorld().playSound(tempSplash, Sound.ENTITY_LIGHTNING_BOLT_IMPACT, 0.25f, 1.0f);
-        pl.getWorld().spigot().strikeLightningEffect(tempSplash, true);
-
-        return loc;
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        if (!fateCasters.contains(e.getPlayer().getUniqueId())) return;
+        if (e.getTo() == null) return;
+        if (!e.getFrom().toVector().equals(e.getTo().toVector())) e.setCancelled(true);
     }
 
-    /**
-     *
-     * @param player
-     * @param ringLoc
-     */
-    private void invertPlayers(Player player, Location ringLoc) {
-        for (Entity en : player.getWorld().getNearbyEntities(ringLoc, RADIUS, RADIUS, RADIUS)) {
-            if (!verifyEnemy(player, en)) continue;
-            if (!(en instanceof Player)) {
-                addStatusEffect(en, EffectEnum.SILENCE, DURATION);
-            } else {
-                invertedPlayers.add(en.getUniqueId());
-                // particles, sounds
-            }
-        }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onHeal(SpellHealEvent e) {
+        if (!invertedEntities.containsKey(e.getEntity().getUniqueId())) return;
+        e.setCancelled(true);
+        DamageUtil.damageEntitySpell(e.getAmount(), (LivingEntity) e.getEntity(), invertedEntities.get(e.getEntity().getUniqueId()));
     }
-    // todo: heal event?
-    // todo: custom regen event
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onRegen(HealthRegenEvent e) {
+        if (!invertedEntities.containsKey(e.getPlayer().getUniqueId())) return;
+        e.setCancelled(true);
+        DamageUtil.damageEntitySpell(e.getAmount(), e.getPlayer(), invertedEntities.get(e.getPlayer().getUniqueId()));
+    }
 }
-
