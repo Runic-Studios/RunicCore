6386d2b52e2a02bea3e96214c2689bb9bd48ab10
Skyfallin <dimetrius.hightower@gmail.com>
2021-12-10 14:07:52 -0700

    added sebaths artifacts! (5/35)

---
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/artifact/DrainLife.java
@@ -0,0 +1,32 @@
+package com.runicrealms.plugin.spellapi.spells.artifact;
+
+import com.runicrealms.plugin.classes.ClassEnum;
+import com.runicrealms.plugin.spellapi.spelltypes.Spell;
+import com.runicrealms.plugin.spellapi.spellutil.HealUtil;
+import com.runicrealms.runicitems.item.event.RunicItemArtifactTriggerEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DrainLife extends Spell {
+
+    private static final String ARTIFACT_ID = "bloodmoon";
+    private static final int HEAL_AMOUNT = 10;
+    private static final double CHANCE = 0.25;
+
+    public DrainLife() {
+        super("Drain Life", "", ChatColor.WHITE, ClassEnum.MAGE, 0, 0);
+        this.setIsPassive(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST) // first
+    public void onArtifactUse(RunicItemArtifactTriggerEvent e) {
+        if (!e.getRunicItemArtifact().getTemplateId().equals(ARTIFACT_ID)) return;
+        double roll = ThreadLocalRandom.current().nextDouble();
+        if (roll > CHANCE) return;
+        HealUtil.healPlayer(HEAL_AMOUNT, e.getPlayer(), e.getPlayer(), false);
+    }
+}
+
---
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/artifact/LifeInfusion.java
@@ -0,0 +1,60 @@
+package com.runicrealms.plugin.spellapi.spells.artifact;
+
+import com.runicrealms.plugin.classes.ClassEnum;
+import com.runicrealms.plugin.spellapi.spelltypes.Spell;
+import com.runicrealms.plugin.spellapi.spellutil.HealUtil;
+import com.runicrealms.runicitems.item.event.RunicItemArtifactTriggerEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import java.util.Objects;
+
+public class LifeInfusion extends Spell {
+
+    private static final String ARTIFACT_ID = "crimson-maul";
+    private static final int HEAL_AMOUNT = 20;
+
+    public LifeInfusion() {
+        super("Life Infusion", "", ChatColor.WHITE, ClassEnum.CLERIC, 0, 0);
+        this.setIsPassive(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST) // first
+    public void onArtifactUse(RunicItemArtifactTriggerEvent e) {
+        if (!e.getRunicItemArtifact().getTemplateId().equals(ARTIFACT_ID)) return;
+        if (e.getVictim() == null) return;
+        if (!(e.getVictim() instanceof LivingEntity)) return;
+        spawnSphere(e.getVictim().getLocation());
+        HealUtil.healPlayer(HEAL_AMOUNT, e.getPlayer(), e.getPlayer(), false);
+    }
+
+    private void spawnSphere(Location location) {
+        for (double i = 0; i <= Math.PI; i += Math.PI / 12) {
+            double radius = Math.sin(i);
+            double y = Math.cos(i);
+            for (double a = 0; a < Math.PI * 2; a += Math.PI / 12) {
+                double x = .9 * Math.cos(a) * radius;
+                double z = .9 * Math.sin(a) * radius;
+                location.add(x, y, z);
+                Objects.requireNonNull(location.getWorld()).spawnParticle
+                        (
+                                Particle.REDSTONE,
+                                location,
+                                1,
+                                0,
+                                0,
+                                0,
+                                0,
+                                new Particle.DustOptions(Color.RED, 3)
+                        );
+                location.subtract(x, y, z);
+            }
+        }
+    }
+}
+
