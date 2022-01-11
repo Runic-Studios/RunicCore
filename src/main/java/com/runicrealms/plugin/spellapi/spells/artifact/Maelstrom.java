f73000b8917c0f84c6d71edaa0751b5bb989abd1
skyfallin <dimetrius.hightower@gmail.com>
2022-01-10 19:07:54 -0700

    updating jorundr mage artifact spell

--- src/main/java/com/runicrealms/plugin/spellapi/spells/artifact/Maelstrom.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/artifact/Maelstrom.java
@@ -1,29 +1,30 @@
 package com.runicrealms.plugin.spellapi.spells.artifact;
 
-import com.runicrealms.plugin.RunicCore;
 import com.runicrealms.plugin.api.RunicCoreAPI;
 import com.runicrealms.plugin.classes.ClassEnum;
 import com.runicrealms.plugin.spellapi.spelltypes.ArtifactSpell;
+import com.runicrealms.plugin.spellapi.spelltypes.EffectEnum;
 import com.runicrealms.plugin.spellapi.spelltypes.Spell;
 import com.runicrealms.plugin.utilities.DamageUtil;
 import com.runicrealms.runicitems.item.event.RunicArtifactOnHitEvent;
 import com.runicrealms.runicitems.item.event.RunicItemArtifactTriggerEvent;
-import org.bukkit.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Maelstrom extends Spell implements ArtifactSpell {
 
-    private static final int DURATION = 4;
-    private static final int RADIUS = 3;
-    private static final double CHANCE = 0.35;
-    private static final double DAMAGE_PERCENT = 0.75;
+    private static final int DURATION = 3;
+    private static final int RADIUS = 2;
+    private static final double CHANCE = 0.25;
+    private static final double DAMAGE_PERCENT = 2.0;
     private static final String ARTIFACT_ID = "runeforged-scepter";
 
     public Maelstrom() {
@@ -40,45 +41,28 @@ public class Maelstrom extends Spell implements ArtifactSpell {
         double roll = ThreadLocalRandom.current().nextDouble();
         if (roll > getChance()) return;
         int damage = (int) ((e.getRunicItemArtifact().getWeaponDamage().getRandomValue() * DAMAGE_PERCENT) + RunicCoreAPI.getPlayerStrength(e.getPlayer().getUniqueId()));
-        maelstromTask(e.getPlayer(), onHitEvent.getVictim(), damage);
+        createMaelstrom(e.getPlayer(), onHitEvent.getVictim(), damage);
         e.setArtifactSpellToCast(this);
     }
 
-    private void maelstromTask(Player player, Entity victim, int damage) {
-        Location castLocation = victim.getLocation();
-        new BukkitRunnable() {
-            int count = 1;
-
-            @Override
-            public void run() {
-                if (count > DURATION) {
-                    this.cancel();
-                } else {
-                    createCircle(player, castLocation);
-                    player.getWorld().playSound(castLocation, Sound.ENTITY_CAT_HISS, 0.5f, 0.1f);
-                    for (Entity en : player.getWorld().getNearbyEntities(castLocation, RADIUS, RADIUS, RADIUS)) {
-                        if (!(verifyEnemy(player, en))) continue;
-                        LivingEntity victim = (LivingEntity) en;
-                        victim.getWorld().playSound(victim.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 0.5f, 2.0f);
-                        DamageUtil.damageEntitySpell(damage, victim, player);
-                    }
-                    count += 1;
-                }
-            }
-        }.runTaskTimer(RunicCore.getInstance(), 0, 20L);
-    }
-
-    private void createCircle(Player player, Location location) {
-        int particles = 50;
-        for (int i = 0; i < particles; i++) {
-            double angle, x, z;
-            angle = 2 * Math.PI * i / particles;
-            x = Math.cos(angle) * (float) RADIUS;
-            z = Math.sin(angle) * (float) RADIUS;
-            location.add(x, 0, z);
-            player.getWorld().spawnParticle(Particle.CRIT_MAGIC, location, 5, 0, 0, 0, 0);
-            player.getWorld().spawnParticle(Particle.REDSTONE, location, 5, 0, 0, 0, 0, new Particle.DustOptions(Color.NAVY, 1));
-            location.subtract(x, 0, z);
+    /**
+     * Create a maelstrom effect at the location of the victim
+     *
+     * @param player who cast the spell
+     * @param victim to be stunned
+     * @param damage of the spell
+     */
+    private void createMaelstrom(Player player, Entity victim, int damage) {
+        player.getWorld().playSound(victim.getLocation(), Sound.ENTITY_CAT_HISS, 0.5f, 0.1f);
+        player.getWorld().playSound(victim.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 0.5f, 0.5f);
+        for (Entity en : player.getWorld().getNearbyEntities(victim.getLocation(), RADIUS, RADIUS, RADIUS)) {
+            if (!(verifyEnemy(player, en))) continue;
+            LivingEntity livingEntity = (LivingEntity) en;
+            livingEntity.getWorld().playSound(livingEntity.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 0.5f, 2.0f);
+            livingEntity.getWorld().spawnParticle(Particle.CRIT_MAGIC, livingEntity.getLocation(), 25, 0.5f, 0.5f, 0.5f, 0);
+            DamageUtil.damageEntitySpell(damage, livingEntity, player);
+            if (!(livingEntity instanceof Player)) // doesn't stun players
+                addStatusEffect(livingEntity, EffectEnum.STUN, DURATION);
         }
     }
 
