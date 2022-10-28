ce34586628545f15a1374b8ded598bce5820582c
skyfallin <dimetrius.hightower@gmail.com>
2022-10-28 13:11:10 -0400

    RR-111 slight bugfix for mob damage

--- src/main/java/com/runicrealms/plugin/listeners/EnvironmentDamageListener.java
+++ src/main/java/com/runicrealms/plugin/listeners/EnvironmentDamageListener.java
@@ -3,6 +3,7 @@ package com.runicrealms.plugin.listeners;
 import com.runicrealms.plugin.events.GenericDamageEvent;
 import com.runicrealms.plugin.utilities.DamageEventUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,11 +14,7 @@ public class EnvironmentDamageListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST) // very early
     public void onEnvironmentDamage(EntityDamageEvent e) {
-        if (!(e.getEntity() instanceof Player)) {
-            e.setCancelled(true);
-            return;
-        }
-        Player player = (Player) e.getEntity();
+        Entity entity = e.getEntity();
         switch (e.getCause()) {
             case CONTACT:
             case DROWNING:
@@ -26,14 +23,16 @@ public class EnvironmentDamageListener implements Listener {
             case HOT_FLOOR:
             case LAVA:
                 e.setCancelled(true);
-                GenericDamageEvent.DamageCauses damageCauses = GenericDamageEvent.DamageCauses.getFromDamageCause(e.getCause());
-                GenericDamageEvent genericDamageEvent = new GenericDamageEvent
-                        (
-                                player,
-                                DamageEventUtil.calculateRunicDamageFromVanillaDamage(player, e.getDamage(), damageCauses),
-                                damageCauses
-                        );
-                Bukkit.getPluginManager().callEvent(genericDamageEvent);
+                if (entity instanceof Player) {
+                    GenericDamageEvent.DamageCauses damageCauses = GenericDamageEvent.DamageCauses.getFromDamageCause(e.getCause());
+                    GenericDamageEvent genericDamageEvent = new GenericDamageEvent
+                            (
+                                    (Player) entity,
+                                    DamageEventUtil.calculateRunicDamageFromVanillaDamage((Player) entity, e.getDamage(), damageCauses),
+                                    damageCauses
+                            );
+                    Bukkit.getPluginManager().callEvent(genericDamageEvent);
+                }
                 break;
         }
     }
