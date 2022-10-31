f5b6ef58202ac407d9b49609710afef8a9871172
Skyfallin <skyfallin.business@gmail.com>
2022-10-31 14:18:53 -0400

    RR-111 adding SessionDataNested.java for more complex jedis objects

---
+++ src/main/java/com/runicrealms/plugin/model/SessionDataNested.java
@@ -0,0 +1,38 @@
+package com.runicrealms.plugin.model;
+
+import com.runicrealms.plugin.database.PlayerMongoData;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
+
+/**
+ * This interface is a slightly different variant of SessionData for data modeling that is embedded deeply in jedis
+ * Current plugin uses: items, achievements, quests
+ *
+ * @author Skyfallin
+ */
+public interface SessionDataNested {
+
+    /**
+     * Different implementation of toMap that allows for a nested object
+     *
+     * @return a map of key-value pairs of data on the nested object
+     */
+    Map<String, String> toMap(Object nestedObject);
+
+    /**
+     * Ensures that all session data has a method to save the data in jedis
+     *
+     * @param jedis the jedis resource
+     * @param slot  an optional argument to represent the character slot (for alt-specific data)
+     */
+    void writeToJedis(Jedis jedis, int... slot);
+
+    /**
+     * Ensure that all session data has a method to save the data in mongo
+     *
+     * @param playerMongoData the mongo data of the player
+     * @param slot            an optional argument to represent the character slot (for alt-specific data)
+     */
+    void writeToMongo(PlayerMongoData playerMongoData, int... slot);
+}
