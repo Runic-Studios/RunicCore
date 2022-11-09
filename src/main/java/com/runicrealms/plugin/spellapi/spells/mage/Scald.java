ddc18c3045acd155ba606dc1b7e87afd40398bb7
Skyfallin <skyfallin.business@gmail.com>
2022-11-09 13:38:49 -0500

    RR-111 more pyromancer QoL updates

--- src/main/java/com/runicrealms/plugin/spellapi/spells/mage/Scald.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/mage/Scald.java
@@ -1,8 +1,14 @@
 package com.runicrealms.plugin.spellapi.spells.mage;
 
 import com.runicrealms.plugin.classes.ClassEnum;
+import com.runicrealms.plugin.events.MagicDamageEvent;
 import com.runicrealms.plugin.spellapi.spelltypes.Spell;
+import com.runicrealms.plugin.utilities.DamageUtil;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 
 /**
  * Logic for hit found in Fireball.
@@ -21,12 +27,15 @@ public class Scald extends Spell {
         this.setIsPassive(true);
     }
 
-    public static double getDamagePercent() {
-        return DAMAGE_PERCENT;
-    }
-
-    public static int getRadius() {
-        return RADIUS;
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onMagicDamage(MagicDamageEvent event) {
+        if (event.isCancelled()) return;
+        if (!hasPassive(event.getPlayer().getUniqueId(), this.getName())) return;
+        if (!(event.getSpell() instanceof Fireball)) return;
+        for (Entity entity : event.getVictim().getNearbyEntities(RADIUS, RADIUS, RADIUS)) {
+            if (!isValidEnemy(event.getPlayer(), entity)) continue;
+            DamageUtil.damageEntitySpell(event.getAmount() * DAMAGE_PERCENT, (LivingEntity) entity, event.getPlayer());
+        }
     }
 }
 
