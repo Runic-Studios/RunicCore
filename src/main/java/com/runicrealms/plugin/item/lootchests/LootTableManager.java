4d5690ae50fda8c45c75e7fe2b3a987a3f62848a
Skyfallin <skyfallin.business@gmail.com>
2023-01-15 17:34:16 -0500

    RR-111 adding ores, flowers to loot chests

--- src/main/java/com/runicrealms/plugin/item/lootchests/LootTableManager.java
+++ src/main/java/com/runicrealms/plugin/item/lootchests/LootTableManager.java
@@ -62,9 +62,13 @@ public class LootTableManager implements LootTableAPI {
         ChestItem oakWood = new ChestItem("OakWood", 3, 5);
         ChestItem thread = new ChestItem("Thread", 3, 5);
         ChestItem animalHide = new ChestItem("AnimalHide", 3, 5);
-        ChestItem uncutRuby = new ChestItem("uncut-ruby", 2, 3);
+        ChestItem uncutRuby = new ChestItem("uncut-ruby", 1, 6);
+        ChestItem goldOre = new ChestItem("gold-ore", 1, 6);
+        ChestItem ironOre = new ChestItem("iron-ore", 1, 6);
         ChestItem bottle = new ChestItem("Bottle", 2, 3);
         ChestItem salmon = new ChestItem("Salmon", 2, 3);
+        ChestItem comfrey = new ChestItem("Comfrey", 2, 3);
+        ChestItem petunia = new ChestItem("Petunia", 2, 3);
 
         // potions
         ChestItem healthPotion = new ChestItem("minor-potion-healing", 1, 1);
@@ -80,8 +84,12 @@ public class LootTableManager implements LootTableAPI {
         LOOT_TABLE_TIER_I.addEntry(thread, 8.0);
         LOOT_TABLE_TIER_I.addEntry(animalHide, 8.0);
         LOOT_TABLE_TIER_I.addEntry(uncutRuby, 8.0);
+        LOOT_TABLE_TIER_I.addEntry(goldOre, 8.0);
+        LOOT_TABLE_TIER_I.addEntry(ironOre, 8.0);
         LOOT_TABLE_TIER_I.addEntry(bottle, 16.0);
         LOOT_TABLE_TIER_I.addEntry(salmon, 8.0);
+        LOOT_TABLE_TIER_I.addEntry(comfrey, 8.0);
+        LOOT_TABLE_TIER_I.addEntry(petunia, 8.0);
 
         LOOT_TABLE_TIER_I.addEntry(healthPotion, 20.0);
         LOOT_TABLE_TIER_I.addEntry(manaPotion, 20.0);
@@ -105,10 +113,16 @@ public class LootTableManager implements LootTableAPI {
         ChestItem oakWood = new ChestItem("OakWood", 3, 5);
         ChestItem thread = new ChestItem("Thread", 3, 5);
         ChestItem animalHide = new ChestItem("AnimalHide", 3, 5);
-        ChestItem uncutRuby = new ChestItem("uncut-ruby", 2, 3);
-        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 2, 3);
+        ChestItem uncutRuby = new ChestItem("uncut-ruby", 3, 8);
+        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 3, 8);
+        ChestItem goldOre = new ChestItem("gold-ore", 3, 6);
+        ChestItem ironOre = new ChestItem("iron-ore", 3, 6);
         ChestItem bottle = new ChestItem("Bottle", 3, 5);
         ChestItem cod = new ChestItem("Cod", 2, 3);
+        ChestItem comfrey = new ChestItem("Comfrey", 2, 3);
+        ChestItem petunia = new ChestItem("Petunia", 2, 3);
+        ChestItem turmeric = new ChestItem("Turmeric", 2, 3);
+        ChestItem psyllium = new ChestItem("Psyllium", 2, 3);
 
         // potions
         ChestItem healthPotion = new ChestItem("major-potion-healing", 1, 1);
@@ -126,8 +140,14 @@ public class LootTableManager implements LootTableAPI {
         LOOT_TABLE_TIER_II.addEntry(animalHide, 8.0);
         LOOT_TABLE_TIER_II.addEntry(uncutRuby, 8.0);
         LOOT_TABLE_TIER_II.addEntry(uncutSapphire, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(goldOre, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(ironOre, 8.0);
         LOOT_TABLE_TIER_II.addEntry(bottle, 16.0);
         LOOT_TABLE_TIER_II.addEntry(cod, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(comfrey, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(petunia, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(turmeric, 8.0);
+        LOOT_TABLE_TIER_II.addEntry(psyllium, 8.0);
 
         LOOT_TABLE_TIER_II.addEntry(healthPotion, 25.0);
         LOOT_TABLE_TIER_II.addEntry(manaPotion, 25.0);
@@ -151,11 +171,19 @@ public class LootTableManager implements LootTableAPI {
         ChestItem oakWood = new ChestItem("OakWood", 3, 5);
         ChestItem thread = new ChestItem("Thread", 3, 5);
         ChestItem animalHide = new ChestItem("AnimalHide", 3, 5);
-        ChestItem uncutRuby = new ChestItem("uncut-ruby", 2, 3);
-        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 2, 3);
-        ChestItem uncutOpal = new ChestItem("uncut-opal", 2, 3);
+        ChestItem uncutRuby = new ChestItem("uncut-ruby", 3, 8);
+        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 3, 8);
+        ChestItem uncutOpal = new ChestItem("uncut-opal", 3, 8);
+        ChestItem goldOre = new ChestItem("gold-ore", 3, 8);
+        ChestItem ironOre = new ChestItem("iron-ore", 3, 8);
         ChestItem bottle = new ChestItem("Bottle", 3, 5);
         ChestItem tropical = new ChestItem("Tropical", 2, 3);
+        ChestItem valerian = new ChestItem("Valerian", 2, 3);
+        ChestItem snowdrop = new ChestItem("Snowdrop", 2, 3);
+        ChestItem chamomile = new ChestItem("Chamomile", 2, 3);
+        ChestItem wintercress = new ChestItem("Wintercress", 2, 3);
+        ChestItem hibiscus = new ChestItem("Hibiscus", 2, 3);
+        ChestItem lavender = new ChestItem("Lavender", 2, 3);
 
         // potions
         ChestItem healthPotion = new ChestItem("major-potion-healing", 1, 1);
@@ -173,8 +201,16 @@ public class LootTableManager implements LootTableAPI {
         LOOT_TABLE_TIER_III.addEntry(uncutRuby, 8.0);
         LOOT_TABLE_TIER_III.addEntry(uncutSapphire, 8.0);
         LOOT_TABLE_TIER_III.addEntry(uncutOpal, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(goldOre, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(ironOre, 8.0);
         LOOT_TABLE_TIER_III.addEntry(bottle, 16.0);
         LOOT_TABLE_TIER_III.addEntry(tropical, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(valerian, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(snowdrop, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(chamomile, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(wintercress, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(hibiscus, 8.0);
+        LOOT_TABLE_TIER_III.addEntry(lavender, 8.0);
 
         LOOT_TABLE_TIER_III.addEntry(healthPotion, 30.0);
         LOOT_TABLE_TIER_III.addEntry(manaPotion, 30.0);
@@ -198,13 +234,17 @@ public class LootTableManager implements LootTableAPI {
         ChestItem oakWood = new ChestItem("OakWood", 3, 5);
         ChestItem thread = new ChestItem("Thread", 3, 5);
         ChestItem animalHide = new ChestItem("AnimalHide", 3, 5);
-        ChestItem uncutRuby = new ChestItem("uncut-ruby", 2, 3);
-        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 2, 3);
-        ChestItem uncutOpal = new ChestItem("uncut-opal", 2, 3);
-        ChestItem uncutEmerald = new ChestItem("uncut-emerald", 2, 3);
-        ChestItem uncutDiamond = new ChestItem("uncut-diamond", 2, 3);
+        ChestItem uncutRuby = new ChestItem("uncut-ruby", 3, 8);
+        ChestItem uncutSapphire = new ChestItem("uncut-sapphire", 3, 8);
+        ChestItem uncutOpal = new ChestItem("uncut-opal", 3, 8);
+        ChestItem uncutEmerald = new ChestItem("uncut-emerald", 3, 8);
+        ChestItem uncutDiamond = new ChestItem("uncut-diamond", 3, 8);
+        ChestItem goldOre = new ChestItem("gold-ore", 3, 8);
+        ChestItem ironOre = new ChestItem("iron-ore", 3, 8);
         ChestItem bottle = new ChestItem("Bottle", 3, 5);
         ChestItem pufferfish = new ChestItem("Pufferfish", 2, 3);
+        ChestItem boswellia = new ChestItem("Boswellia", 2, 3);
+        ChestItem arugula = new ChestItem("Arugula", 2, 3);
 
         // potions
         ChestItem healthPotion = new ChestItem("greater-potion-healing", 1, 1);
@@ -224,6 +264,10 @@ public class LootTableManager implements LootTableAPI {
         LOOT_TABLE_TIER_IV.addEntry(uncutOpal, 8.0);
         LOOT_TABLE_TIER_IV.addEntry(uncutEmerald, 8.0);
         LOOT_TABLE_TIER_IV.addEntry(uncutDiamond, 8.0);
+        LOOT_TABLE_TIER_IV.addEntry(goldOre, 8.0);
+        LOOT_TABLE_TIER_IV.addEntry(ironOre, 8.0);
+        LOOT_TABLE_TIER_IV.addEntry(boswellia, 8.0);
+        LOOT_TABLE_TIER_IV.addEntry(arugula, 8.0);
 
         LOOT_TABLE_TIER_IV.addEntry(bottle, 16.0);
         LOOT_TABLE_TIER_IV.addEntry(pufferfish, 8.0);
