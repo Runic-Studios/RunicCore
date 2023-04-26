b550a832d8e2f03379e11411c5db057bbe93a9f7
Skyfallin <dimetrius.hightower@gmail.com>
2023-04-26 14:54:26 -0400

    RR-347 Paladin complete!

--- src/main/java/com/runicrealms/plugin/spellapi/spells/warrior/Smite.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/warrior/Smite.java
@@ -1,20 +1,123 @@
 package com.runicrealms.plugin.spellapi.spells.warrior;
 
 import com.runicrealms.plugin.classes.CharacterClass;
-import com.runicrealms.plugin.spellapi.spelltypes.Spell;
-import com.runicrealms.plugin.spellapi.spelltypes.SpellItemType;
+import com.runicrealms.plugin.spellapi.spelltypes.*;
+import com.runicrealms.plugin.spellapi.spellutil.VectorUtil;
+import com.runicrealms.plugin.utilities.DamageUtil;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
 
-public class Smite extends Spell {
+import java.util.Map;
+
+public class Smite extends Spell implements DistanceSpell, MagicDamageSpell, RadiusSpell {
+    private static final double BEAM_WIDTH = 1.5;
+    private double maxDistance;
+    private double damage;
+    private double damagePerLevel;
+    private double knockback;
+    private double radius;
 
     public Smite() {
         super("Smite", CharacterClass.WARRIOR);
-        this.setDescription("?");
+        this.setDescription("You fire a beam of light, " +
+                "dealing (" + damage + " + &f" + damagePerLevel
+                + "x&7 lvl) magicʔ damage to the first enemy hit " +
+                "and knocking away all enemies within " + radius + " blocks!");
     }
-
 
     @Override
     public void executeSpell(Player player, SpellItemType type) {
-        // ?
+        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 0.5f, 1.0f);
+        RayTraceResult rayTraceResult = player.getWorld().rayTraceEntities
+                (
+                        player.getLocation(),
+                        player.getLocation().getDirection(),
+                        maxDistance,
+                        BEAM_WIDTH,
+                        entity -> isValidEnemy(player, entity)
+                );
+
+        if (rayTraceResult == null) {
+            Location location = player.getTargetBlock(null, (int) maxDistance).getLocation();
+            VectorUtil.drawLine(player, Particle.CLOUD, Color.WHITE, player.getEyeLocation(), location, 0.5D, 1, 0.25f);
+            player.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, location, 8, 0.5f, 0.5f, 0.5f, 0);
+        } else if (rayTraceResult.getHitEntity() != null) {
+            LivingEntity livingEntity = (LivingEntity) rayTraceResult.getHitEntity();
+            VectorUtil.drawLine(player, Particle.CLOUD, Color.WHITE, player.getEyeLocation(), livingEntity.getEyeLocation(), 0.75D, 1, 0.15f);
+            livingEntity.getWorld().playSound(livingEntity.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 0.25f, 2.0f);
+            livingEntity.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, livingEntity.getEyeLocation(), 8, 0.8f, 0.5f, 0.8f, 0);
+            for (Entity entity : livingEntity.getWorld().getNearbyEntities(livingEntity.getLocation(), radius, radius, radius, target -> isValidEnemy(player, target))) {
+                // Calculate knockback direction
+                Vector attackerPos = player.getLocation().toVector();
+                Vector enemyPos = entity.getLocation().toVector();
+                Vector knockbackDirection = enemyPos.subtract(attackerPos).normalize();
+                // Apply knockback to enemy
+                Vector knockbackVector = knockbackDirection.multiply(knockback);
+                knockbackVector.setY(knockbackVector.getY() + 0.15);
+                entity.setVelocity(entity.getVelocity().add(knockbackVector));
+            }
+            DamageUtil.damageEntitySpell(damage, livingEntity, player, this);
+        }
     }
+
+    @Override
+    public double getDistance() {
+        return maxDistance;
+    }
+
+    @Override
+    public void setDistance(double distance) {
+        this.maxDistance = distance;
+    }
+
+    @Override
+    public void loadDistanceData(Map<String, Object> spellData) {
+        Number distance = (Number) spellData.getOrDefault("distance", 0);
+        setDistance(distance.doubleValue());
+        Number knockback = (Number) spellData.getOrDefault("knockback", 0);
+        setKnockback(knockback.doubleValue());
+    }
+
+    @Override
+    public double getMagicDamage() {
+        return damage;
+    }
+
+    @Override
+    public void setMagicDamage(double magicDamage) {
+        this.damage = magicDamage;
+    }
+
+    @Override
+    public double getMagicDamagePerLevel() {
+        return damagePerLevel;
+    }
+
+    @Override
+    public void setMagicDamagePerLevel(double magicDamagePerLevel) {
+        this.damagePerLevel = magicDamagePerLevel;
+    }
+
+    @Override
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setKnockback(double knockback) {
+        this.knockback = knockback;
+    }
+
 }
+
