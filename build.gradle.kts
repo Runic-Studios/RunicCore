95ab3caa08435d5562a483d249148fa06317d83a
Excel619 <excel8392@gmail.com>
2023-05-20 23:48:10 -0600

    Updated buildscript and plugin.yml dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 group = "com.runicrealms.plugin"
 version = "1.0-SNAPSHOT"
+val artifactName = "core"
 
 dependencies {
     compileOnly(commonLibs.worldguardevents)
@@ -46,12 +47,9 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.runicrealms.plugin"
-            artifactId = "core"
+            artifactId = artifactName
             version = "1.0-SNAPSHOT"
             from(components["java"])
         }
     }
 }
-
-tasks.register("wrapper")
-tasks.register("prepareKotlinBuildScriptModel")
