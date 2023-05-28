410959326c7804cb6268c8b85645dfda9d81e717
Skyfallin <dimetrius.hightower@gmail.com>
2023-05-28 08:25:30 -0400

    Offhand tridents now scrap into iron bars

--- src/main/java/com/runicrealms/plugin/ItemType.java
+++ src/main/java/com/runicrealms/plugin/ItemType.java
@@ -4,25 +4,36 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ItemType {
 
     CLOTH, LEATHER, GILDED, MAIL, PLATE, GEMSTONE, MAINHAND, OFFHAND, CONSUMABLE, ARCHER, CLERIC, MAGE, ROGUE, WARRIOR, AIR;
+
+    public static final List<Integer> OFFHAND_ITEM_IDS = new ArrayList<>();
+
+    static {
+        OFFHAND_ITEM_IDS.add(235);
+        OFFHAND_ITEM_IDS.add(236);
+        OFFHAND_ITEM_IDS.add(237);
+    }
 
     public static ItemType matchType(final ItemStack itemStack) {
         if (itemStack == null) {
             return null;
         }
         if (itemStack.getType() == Material.SHEARS) {
-            return switch (((Damageable) itemStack.getItemMeta()).getDamage()) {
-                case 235, 236, 237 -> OFFHAND;
-                default -> CLOTH;
-            };
+            if (itemStack.getItemMeta() == null) return CLOTH;
+            if (OFFHAND_ITEM_IDS.contains(((Damageable) itemStack.getItemMeta()).getDamage()))
+                return OFFHAND;
+            else
+                return CLOTH;
         } else {
             return switch (itemStack.getType()) {
                 case IRON_HELMET, IRON_CHESTPLATE, IRON_LEGGINGS, IRON_BOOTS -> PLATE;
                 case GOLDEN_HELMET, GOLDEN_CHESTPLATE, GOLDEN_LEGGINGS, GOLDEN_BOOTS -> GILDED;
-                case CHAINMAIL_HELMET, CHAINMAIL_CHESTPLATE, CHAINMAIL_LEGGINGS, CHAINMAIL_BOOTS ->
-                        MAIL;
+                case CHAINMAIL_HELMET, CHAINMAIL_CHESTPLATE, CHAINMAIL_LEGGINGS, CHAINMAIL_BOOTS -> MAIL;
                 case LEATHER_HELMET, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_BOOTS -> LEATHER;
                 case DIAMOND_HELMET, DIAMOND_CHESTPLATE, DIAMOND_LEGGINGS, DIAMOND_BOOTS -> CLOTH;
                 case REDSTONE, LAPIS_LAZULI, QUARTZ, EMERALD, DIAMOND -> GEMSTONE;
