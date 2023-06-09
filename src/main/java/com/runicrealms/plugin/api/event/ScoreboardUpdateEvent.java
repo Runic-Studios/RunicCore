ed1b2b836344db5c55697edc49982143bdfa0911
Skyfallin <dimetrius.hightower@gmail.com>
2023-06-09 13:59:20 -0400

    push for redis connection leaks

--- src/main/java/com/runicrealms/plugin/api/event/ScoreboardUpdateEvent.java
+++ src/main/java/com/runicrealms/plugin/api/event/ScoreboardUpdateEvent.java
@@ -1,11 +1,9 @@
 package com.runicrealms.plugin.api.event;
 
-import com.runicrealms.plugin.rdb.RunicDatabase;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.scoreboard.Scoreboard;
-import redis.clients.jedis.Jedis;
 
 /**
  * A custom ASYNC event which is called when a player's scoreboard will update
@@ -13,11 +11,9 @@ import redis.clients.jedis.Jedis;
  * @author Skyfallin
  */
 public class ScoreboardUpdateEvent extends Event {
-
     private static final HandlerList handlers = new HandlerList();
     private final Player player;
     private final Scoreboard scoreboard;
-    private final Jedis jedis;
     private String profession;
     private int professionLevel;
     private boolean isOutlaw;
@@ -30,52 +26,18 @@ public class ScoreboardUpdateEvent extends Event {
         super(true);
         this.player = player;
         this.scoreboard = scoreboard;
-        this.jedis = RunicDatabase.getAPI().getRedisAPI().getNewJedisResource();
         this.profession = "";
         this.professionLevel = 0;
         this.isOutlaw = false;
     }
 
-//    /**
-//     * @param profession      string to set
-//     * @param professionLevel level of profession
-//     */
-//    public ScoreboardUpdateEvent(final Player player, final Scoreboard scoreboard, String profession, int professionLevel) {
-//        super(true);
-//        this.player = player;
-//        this.scoreboard = scoreboard;
-//        this.jedis = RunicCore.getRedisAPI().getNewJedisResource();
-//        this.profession = profession;
-//        this.professionLevel = professionLevel;
-//    }
-//
-//    /**
-//     * @param jedis a preexisting jedis resource
-//     */
-//    public ScoreboardUpdateEvent(final Player player, final Scoreboard scoreboard, Jedis jedis) {
-//        super(true);
-//        this.player = player;
-//        this.scoreboard = scoreboard;
-//        this.jedis = jedis;
-//        this.profession = "";
-//        this.professionLevel = 0;
-//    }
-
     public static HandlerList getHandlerList() {
         return handlers;
-    }
-
-    public void close() {
-        this.jedis.close();
     }
 
     @Override
     public HandlerList getHandlers() {
         return handlers;
-    }
-
-    public Jedis getJedis() {
-        return this.jedis;
     }
 
     public Player getPlayer() {
