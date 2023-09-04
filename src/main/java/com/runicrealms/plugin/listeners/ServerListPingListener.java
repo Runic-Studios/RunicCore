fec165979aa1dd88b83265f71c4467794a61160c
BoBoBalloon <boboballoongaming@gmail.com>
2023-09-04 14:24:10 -0400

    Rivised with Sky

--- src/main/java/com/runicrealms/plugin/listeners/ServerListPingListener.java
+++ src/main/java/com/runicrealms/plugin/listeners/ServerListPingListener.java
@@ -9,8 +9,7 @@ import org.bukkit.event.server.ServerListPingEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class ServerListPingListener implements Listener {
-
-    private static final String PREFIX = ColorUtil.format("                &d&lRUNIC REALMS&r " + RunicCore.VERSION_NUMBER + "\n              ");
+    private static final String PREFIX = ColorUtil.format("                &d&lRunic Realms &r" + RunicCore.VERSION_NUMBER + "&r\n              ");
     private static String SERVER_MOTD;
 
     @EventHandler
