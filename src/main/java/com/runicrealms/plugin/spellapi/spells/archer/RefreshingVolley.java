48d3ee0214a48c70f1c34f0f5c7c31a1da1d1d73
BoBoBalloon <boboballoongaming@gmail.com>
2023-09-26 21:30:06 -0400

    Fixed some spell descriptions

--- src/main/java/com/runicrealms/plugin/spellapi/spells/archer/RefreshingVolley.java
+++ src/main/java/com/runicrealms/plugin/spellapi/spells/archer/RefreshingVolley.java
@@ -22,7 +22,7 @@ public class RefreshingVolley extends Spell implements HealingSpell, DistanceSpe
         this.setIsPassive(true);
         this.setDescription("While &aRapid Fire&7 is active, each time you land a ranged attack on an enemy" +
                 "heal✦ yourself and your closest 2 allies within " + this.distance + " blocks " +
-                "for " + this.heal + " + (&f" + this.healingPerLevel + " x&7 lvl) health.");
+                "for (" + this.heal + " + &f" + this.healingPerLevel + "x&7 lvl) health.");
     }
 
     @Override
