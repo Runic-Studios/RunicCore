def3b31e1a9dc0c01543e9d6230092df29bf50f2
BoBoBalloon <boboballoongaming@gmail.com>
2023-11-26 08:40:07 -0500

    Added distribute command and revised dungeon key checks

--- src/main/java/com/runicrealms/plugin/listeners/WorldChangeListener.java
+++ src/main/java/com/runicrealms/plugin/listeners/WorldChangeListener.java
@@ -1,12 +1,9 @@
 package com.runicrealms.plugin.listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
+import com.runicrealms.plugin.item.shops.RunicItemShopHelper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.inventory.ItemStack;
 
 /**
  * This method clears 'dungeon keys' from player inventories when they leave the dungeon world
@@ -16,27 +13,11 @@ import org.bukkit.inventory.ItemStack;
 public class WorldChangeListener implements Listener {
 
     @EventHandler
-    public void onWorldChange(PlayerChangedWorldEvent e) {
-
-        if (e.getFrom().getName().toLowerCase().equals("dungeons")) {
-
-            Player pl = e.getPlayer();
-
-            ItemStack[] inv = pl.getInventory().getContents();
-
-            boolean hasSentMessage = false;
-            for (ItemStack itemStack : inv) {
-
-                if (itemStack == null) continue;
-                if (!itemStack.hasItemMeta()) continue;
-                if (itemStack.getType() == Material.PRISMARINE_CRYSTALS || itemStack.getType() == Material.PRISMARINE_SHARD) {
-                    itemStack.setAmount(0);
-                    if (!hasSentMessage) {
-                        pl.sendMessage(ChatColor.GRAY + "Your dungeon keys have been removed.");
-                        hasSentMessage = true;
-                    }
-                }
-            }
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        if (!event.getFrom().getName().equalsIgnoreCase("dungeons")) {
+            return;
         }
+
+        RunicItemShopHelper.clearDungeonKeys(event.getPlayer());
     }
 }
