16217aea22b9078fe5177ae3e4465ad1441be24c
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-01-21 10:44:50 -0500

    RR-521 adjusting stonehaven fast travel to be in front of wagonmaster

--- src/main/java/com/runicrealms/plugin/TravelLocation.java
+++ src/main/java/com/runicrealms/plugin/TravelLocation.java
@@ -21,7 +21,7 @@ public enum TravelLocation {
     NAHEEN("naheen", "Naheen", new Location(Bukkit.getWorld("Alterra"), 1861.5, 39, 154.5, 315, 0)),
     NAZMORA("nazmora", "Naz'mora", new Location(Bukkit.getWorld("Alterra"), 2608.5, 33, 998.5, 0, 0)),
     ORC_OUTPOST("orc_outpost", "Orc Outpost", new Location(Bukkit.getWorld("Alterra"), 2428.5, 25, 1754.5, 90, 0)),
-    STONEHAVEN("stonehaven", "Stonehaven", new Location(Bukkit.getWorld("Alterra"), -788.5, 38, 749.5, 90, 0)),
+    STONEHAVEN("stonehaven", "Stonehaven", new Location(Bukkit.getWorld("Alterra"), -830.5, 38, 799.5, 180, 0)),
     AZUREMIST("azuremist", "Azuremist Peak", new Location(Bukkit.getWorld("Alterra"), 350.5, 97, 787.5, 180, 0)),
     FROSTS_END("frosts_end", "Frost's End", new Location(Bukkit.getWorld("Alterra"), 1116.5, 33, 2576.5, 90, 0)),
     /*
