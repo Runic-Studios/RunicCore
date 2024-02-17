9045b6fb0452ca6eec60ced91b1ca67db86a7daa
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-02-17 14:01:34 -0500

    RR-631 fixing minor bug w/ model casting, updating mounts to use ItemUtils.takeItem

--- src/main/java/com/runicrealms/plugin/listeners/ModelInteractListener.java
+++ src/main/java/com/runicrealms/plugin/listeners/ModelInteractListener.java
@@ -1,10 +1,12 @@
 package com.runicrealms.plugin.listeners;
 
 import com.runicrealms.plugin.RunicCore;
+import com.runicrealms.plugin.WeaponType;
 import com.runicrealms.plugin.common.event.ModelInteractEvent;
 import com.runicrealms.plugin.spellapi.SpellSlot;
 import com.runicrealms.plugin.spellapi.event.SpellTriggerEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,6 +18,12 @@ public class ModelInteractListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onModelInteract(ModelInteractEvent event) {
+        Player player = event.getWhoClicked();
+        WeaponType heldItemType = WeaponType.matchType(player.getInventory().getItemInMainHand());
+        if (heldItemType == WeaponType.NONE) return;
+        if (heldItemType == WeaponType.GATHERING_TOOL) return;
+        if (!DamageListener.matchClass(player, false)) return;
+
         // Determine spell slot from event
         SpellSlot spellSlot = event.getInteractionType() == ModelInteractEvent.InteractType.LEFT_CLICK
                 ? SpellSlot.LEFT_CLICK
